d8f7f96d7f357f7edc9cece4f53a635bc39a233b
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-03-21 12:50:21 -0300

    Update Loteria.java

--- Banco/Loteria.java
+++ Banco/Loteria.java
@@ -9,30 +9,24 @@ public class Loteria {
         int premioMaximo = 1000000;
         int quantoGanhou = loteria(premioMaximo);
 
-        System.out.println("Clarice ganhou R$" + quantoGanhou);
-
         Pessoa Clarice = new Pessoa("Clarice", "111222333-44");
 
         Conta contaClarice = new Conta(Clarice) {
             @Override
             public long getNúmero() {
                 return super.getNúmero();
-
-
             }
-        };
+        };   
         contaClarice.depositar(1000);
         contaClarice.depositar(quantoGanhou);
-
+        
+        System.out.println("Clarice ganhou R$" + quantoGanhou);
         System.out.println("Novo saldo: R$" + contaClarice.getSaldo());
-
 
     }
     private static int loteria(int premioMaximo) {
 
-
         Random rand = new Random();
-
 
         return rand.nextInt(premioMaximo);
     }
