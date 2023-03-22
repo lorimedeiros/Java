320444995e440aceb23f200fde7bf9e3cf85b019
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-03-22 12:20:31 -0300

    Create informaQtParesImpares.java
    questão que caiu na minha prova, "Receber 10 números e informar quantidade de pares e ímpares".

---
+++ informaQtParesImpares.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ParesImpares {
+    public static void main(String[] args) {
+
+        int numImp = 0;
+        int numPar = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0; i < 10; i++){
+
+            int num = Integer.parseInt(sc.nextLine());
+
+            if (num % 2 == 0) {
+                numPar++;
+            } else {
+                numImp++;
+            }
+
+        }
+
+        System.out.println("Foram informados " + numPar + " números pares");
+        System.out.println("Foram informados " + numImp + " números ímpares");
+
+        sc.close();
+
+    }
+}
