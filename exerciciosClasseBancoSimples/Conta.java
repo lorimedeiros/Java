df60728faba56b2c247a39abd17eee8e3a72f1a8
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-07-30 16:06:58 -0300

    Create Conta.java

---
+++ exerciciosClasseBancoSimples/Conta.java
@@ -0,0 +1,25 @@
+public class Conta {
+
+    private int numeroConta;
+    public String nomeTitular;
+    private Double saldo;
+
+    public void Conta(int numeroConta, String nomeTitular, double saldo){
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+        this.saldo = saldo;
+    }
+
+    public void deposita(double valor){
+        saldo += valor;
+    }
+
+    public void saca(double valor){
+        saldo -= (valor + 5);
+    }
+
+    public String toString(){
+        return "Conta " + numeroConta + ", Titular: " + nomeTitular + ", Saldo: R$ " + saldo;
+    }
+
+}
