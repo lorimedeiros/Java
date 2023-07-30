2e9cda4970639d3b9f722434be419e81e5f8307d
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-07-30 12:30:46 -0300

    Update Estudante.java

--- classeEstudante/Estudante.java
+++ classeEstudante/Estudante.java
@@ -4,13 +4,13 @@ public class Estudante {
     public double nota1;
     public double nota2;
     public double nota3;
-    public double somaDasNotas;
+    
     public double somaDasNotas(){
-        return somaDasNotas = nota1 + nota2 + nota3;
+        return nota1 + nota2 + nota3;
     }
 
     public String resultado(){
-        if (somaDasNotas >= 60){
+        if (somaDasNotas() >= 60){
             return "Passou";
         } else {
             return "Reprovou\nFaltaram " + (60 - somaDasNotas) + " pontos";
