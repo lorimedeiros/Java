1dee1456a224bcd1e5aba6c47c93a508bea0a5b7
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-08-21 10:01:33 -0300

    Add files via upload

---
+++ Disciplina de Linguagem de Programação/BlocoDeNotasComExemploDeFacade/src/Facade.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+
+public class Facade {
+
+    //faz o método aqui e no main so chama o método, afim de "proteger" as movimentações
+
+    public void menuPrincipal(){
+
+        Scanner sc = new Scanner(System.in);
+        BlocoDeNotas bn = new BlocoDeNotas();
+        Boolean parada = false;
+
+        System.out.println("Bem vindo ao seu bloco de notas!");
+        System.out.println();
+
+        do {
+
+            System.out.println("Menu:\n" +
+                    "1. Adicionar nota\n" +
+                    "2. Ver bloco de Notas\n" +
+                    "3. Editar nota\n" +
+                    "4. Excluir nota\n" +
+                    "5. Ver lixeira\n" +
+                    "6. Recuperar nota\n" +
+                    "7. Sair");
+
+            int respMenu = Integer.parseInt(sc.nextLine());
+            System.out.println();
+
+            if ((respMenu < 1) || (respMenu > 7)){
+                System.out.println("Opção inexistente");
+                System.out.println();
+            }
+
+            if (respMenu == 1){
+
+                System.out.println("Insira conteúdo da nota");
+                String cn = sc.nextLine();
+                bn.adicionar(cn);
+
+            } else if (respMenu == 2){
+
+                bn.retornarBloco();
+
+            } else if (respMenu == 3){
+
+                System.out.println("Insira o ID da nota que deseja editar:");
+                int id = Integer.parseInt(sc.nextLine());
+
+                if (bn.buscar(id) == null){
+
+                    System.out.println("Nota não encontrada");
+
+                } else {
+
+                    System.out.println("Insira o novo conteúdo da nota:");
+                    String cont = sc.nextLine();
+                    bn.editar(cont, id);
+
+                }
+
+            } else if (respMenu == 4){
+
+                System.out.println("Insira o ID da nota que deseja excluir:");
+                int id = Integer.parseInt(sc.nextLine());
+
+                if (bn.buscar(id) == null){
+
+                    System.out.println("Nota não encontrada");
+
+                } else {
+
+                    bn.remover(id);
+
+                }
+
+            } else if (respMenu == 5){
+
+                bn.retornarLixeira();
+
+            } else if (respMenu == 6){
+
+                System.out.println("Insira o ID, na lixeira, da nota que deseja recuperar:");
+                int id = Integer.parseInt(sc.nextLine());
+
+                if (bn.buscar(id) == null){
+
+                    System.out.println("Nota não encontrada");
+
+                } else {
+
+                    bn.recuperar(id);
+
+                }
+
+            } else if (respMenu == 7){
+
+                parada = true;
+
+            }
+
+        } while(!parada);
+
+        sc.close();
+
+    }
+
+}
