608e1f79d42f14c194c32e3bde3831b3f9390383
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-12-13 09:44:53 -0300

    Update App.java

--- Disciplina APS/Exercicio1(Banco)/App.java
+++ Disciplina APS/Exercicio1(Banco)/App.java
@@ -1,3 +1,18 @@
+/*
+Atividade Banco - Saldo
+Implemente as classes necessárias para as seguintes funcionalidades de um sistema bancário:
+
+Criar uma conta corrente;
+Fazer o crédito de um valor em uma conta corrente;
+Fazer o débito de um valor em uma conta corrente;
+Obter o saldo de uma conta corrente;
+Tanto a conta corrente quanto o correntista pertencem a um Banco.
+Um Banco possui um nome e um código.
+Um Correntista possui nome, cpf e data de nascimento. Uma conta corrente possui um número e um ou mais correntistas.
+
+Observação: os valores financeiros devem usar a classe BigDecimal.
+*/
+
 package br.ufpb.dcx.aps.atividade;
 
 import java.math.BigDecimal;
