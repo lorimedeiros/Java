cd37e4a51ed1a75123573b580ee11bb505536a18
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2023-12-21 21:52:16 -0300

    Update and rename Verificar se caracteres são apenas alfabéticos.java to Verificar se caracteres são apenas alfabéticos ou numéricos.java

--- podeSerUtil/Verificar se caracteres são apenas alfabéticos ou numéricos.java
+++ podeSerUtil/Verificar se caracteres são apenas alfabéticos ou numéricos.java
@@ -3,3 +3,7 @@
 public static boolean isNomeValido(String nome) {
       return nome.matches("[a-zA-Z]+"); // Verifica se o nome contém apenas caracteres alfabéticos
 }
+
+public static boolean isMatriculaValida(String matricula) {
+    return matricula.matches("\\d+"); // Verifica se a matrícula contém APENAS números
+}
