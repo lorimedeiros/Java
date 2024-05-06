c23c41585be4d43b1653e361337a3c802a1eef44
Cecília Medeiros <98926823+cecimedeiros@users.noreply.github.com>
2024-05-06 07:51:05 -0300

    Update Verificar se caracteres são apenas alfabéticos ou numéricos ou espaços.java

--- podeSerUtil/Verificar se caracteres são apenas alfabéticos ou numéricos ou espaços.java
+++ podeSerUtil/Verificar se caracteres são apenas alfabéticos ou numéricos ou espaços.java
@@ -1,5 +1,3 @@
-//O código abaixo retorna se "nome" contém apenas caracteres alfabéticos
-
 public static boolean isNomeValido(String nome) {
       return nome.matches("[a-zA-Z]+"); // Verifica se o nome contém apenas caracteres alfabéticos
 }
