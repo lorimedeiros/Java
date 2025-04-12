7a95a04f3dcebd2460d53d2aa81fe55b71e94c0d
Lori Medeiros <98926823+lorimedeiros@users.noreply.github.com>
2025-04-11 21:26:21 -0300

    Create exemplo conect bean configuration2.java

---
+++ podeSerUtil/exemplo conect bean configuration2.java
@@ -0,0 +1,41 @@
+/*
+definindo valores na mão
+*/
+
+package io.github.lorimedeiros.livraria_api.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    // Valores definidos manualmente (substitua com suas credenciais)
+    private static final String URL = "jdbc:postgresql://localhost:5432/livraria";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String DRIVER = "org.postgresql.Driver";
+
+    @Bean
+    public DataSource hikariDataSource() {
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl(URL);
+        config.setUsername(USERNAME);
+        config.setPassword(PASSWORD);
+        config.setDriverClassName(DRIVER);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("livreria-db-pool");
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(100000);
+        config.setConnectionTestQuery("SELECT 1");
+
+        return new HikariDataSource(config);
+    }
+}
